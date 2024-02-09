b42db55e1c177eba95c9d6d69f499dcc336840aa
Larry <skala.larissa@gmail.com>
2024-02-09 12:59:27 +0100

    hhhh

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,4 @@
-import { MantineProvider } from '@mantine/core';
 import React from 'react';
-import { theme } from '../style';
 import { HomeDiagrams } from '../components/HomeDiagrams';
 import { Text, View } from 'react-native';
 
