8059ff46b0fe480b080ff26c04cfe718c9a5fe55
Larry <skala.larissa@gmail.com>
2024-02-09 11:23:29 +0100

    Dynamic TExtSize

--- style.ts
+++ style.ts
@@ -4,25 +4,21 @@ import React from 'react';
 
 const { width } = Dimensions.get('window')
 const sizeH1= 32;
-export const theme = createTheme({
+const theme = createTheme({
   colors:{
     'backgroundColor': ['#080726', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
     'primaryPink': ['#BE63BF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
     'secondaryPurple': ['#513073', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
 },
 
-  shadows: {
-    md: '1px 1px 3px rgba(0, 0, 0, .25)',
-    xl: '5px 5px 3px rgba(0, 0, 0, .25)',
-  },
-
   headings: {
     fontFamily: 'Roboto, sans-serif',
     sizes: {
-      h1: { fontSize: rem(width / 14) },
-      h2: { fontSize: rem(24) },
-      h3: { fontSize: rem(16) },
-      h4: { fontSize: rem(14) },
+      h1: { fontSize: rem(width / 14),
+            fontWeight: "bold" },
+      h2: { fontSize: rem(width / 18) },
+      h3: { fontSize: rem(width / 27) },
+      h4: { fontSize: rem(width / 31) },
     },
   },
 });
